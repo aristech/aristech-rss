a357097058fddda9ddc53aba1c8488cf13573ea6
Gernard Cerma <aristech.gr@gmail.com>
2018-10-05 16:22:50 +0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -10,7 +10,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 global $wpdb;
-$array = array( 'aristech_options','aristech_title','aristech_max_posts','aristech_radio','aristech_tel','aristech_radio');
+$array = array( 'aristech_options','aristech_title','aristech_max_posts','aristech_radio');
 
 foreach ($array as $item) {
     $item = esc_sql( $item );
